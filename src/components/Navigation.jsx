47b61d929e6568c274af98ae5f7884da597a00fe
unknown <iloriojomode@gmail.com>
2024-08-12 09:21:51 +0100

    jjj

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,37 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Navigation = () => (
-  <nav className="navigation">
-    <div className="nav-title">
-      <h3>Asiwaju</h3>
-    </div>
-    <div className="nav-list">
-      <ul>
-        <li>
-          <a href="#home">Home</a>
-        </li>
-        <li>
-          <a href="#about">About</a>
-        </li>
-        <li>
-          <a href="#projects">Projects</a>
-        </li>
-        <li>
-          <a href="#services">Services</a>
-        </li>
-        <li>
-          <a href="#about">Resume</a>
-        </li>
-      </ul>
-    </div>
-    <div className="nav-button">
-      <a href="mailto:iloriojomodel@gmail.com">
-        <button>
-          <p>Contact Me</p>
-        </button>
-      </a>
-    </div>
-  </nav>
-);
+const Navigation = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="navigation">
+      <div className="nav-title">
+        <h3>Asiwaju</h3>
+      </div>
+      <div className={`nav-list ${isOpen ? "open" : ""}`}>
+        <ul>
+          <li>
+            <a href="#home" onClick={toggleMenu}>
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#about" onClick={toggleMenu}>
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#projects" onClick={toggleMenu}>
+              Projects
+            </a>
+          </li>
+          <li>
+            <a href="#services" onClick={toggleMenu}>
+              Services
+            </a>
+          </li>
+          <li>
+            <a href="#about" onClick={toggleMenu}>
+              Resume
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="nav-button">
+        <a href="mailto:iloriojomodel@gmail.com">
+          <button>
+            <p>Contact Me</p>
+          </button>
+        </a>
+      </div>
+      <div className="hamburger" onClick={toggleMenu}>
+        <div className={`line ${isOpen ? "open" : ""}`}></div>
+        <div className={`line ${isOpen ? "open" : ""}`}></div>
+        <div className={`line ${isOpen ? "open" : ""}`}></div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navigation;
